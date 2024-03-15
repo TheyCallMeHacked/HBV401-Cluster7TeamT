43702bbe56f332dd7b6c9aa50e7078c41c9ef999
Alexandre Klein <alexandre.klein04@protonmail.com>
2024-03-15 10:47:02 +0000

    Add ServiceController interface

---
+++ Assignment 4/src/ServiceController.java
@@ -0,0 +1,7 @@
+public interface ServiceController {
+    public ArrayList<Service> search(str);
+    public ArrayList<Service> recommend(Trip);
+    public void addService(Service);
+    public void removeService(Service);
+    public Booking book(specialNeeds, user);
+}
