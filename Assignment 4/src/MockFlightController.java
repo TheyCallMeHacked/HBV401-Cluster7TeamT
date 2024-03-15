e32d225de70d9602e715a21433e4fa822ef9817c
Alexandre Klein <alexandre.klein04@protonmail.com>
2024-03-15 10:56:26 +0000

    Added mock FlightController

---
+++ Assignment 4/src/MockFlightController.java
@@ -0,0 +1,22 @@
+public class MockFlightController implements ServiceContoller {
+    public ArrayList<Service> search(str) {
+        return new ArrayList<Service>();
+    }
+
+    public ArrayList<Service> recommend(Trip) {
+        return new ArrayList<Service>();
+    }
+
+
+    public void addService(Service) {
+        return;
+    }
+
+    public void removeService(Service) {
+        return;
+    }
+
+    public Booking book(specialNeeds, user) {
+        return new Booking();
+    }
+}
