e561e718aabd75b21a15a924beb35e336ac2d04c
lumeto_de_diamanto <gretar.vidir@gmail.com>
2024-03-20 16:12:36 +0000

    Replaces 'Trip' in interface with 'Service[]'

--- Assignment 4/src/ServiceController.java
+++ Assignment 4/src/ServiceController.java
@@ -1,6 +1,6 @@
 public interface ServiceController {
     public Service[] search(String query);
-    public Service[] recommend(Trip trip);
+    public Service[] recommend(Service[] trip);
     public void addService(Service service);
     public void removeService(Service service);
     public Booking book(specialNeeds sn, User user);
