d84e8d662d6a5efc9dd2e1f6ee77a330a7c3d5db
lumeto_de_diamanto <gretar.vidir@gmail.com>
2024-03-20 16:20:03 +0000

    Added Service parameter to book()

--- Assignment 4/src/ServiceController.java
+++ Assignment 4/src/ServiceController.java
@@ -3,5 +3,5 @@ public interface ServiceController {
     public Service[] recommend(Service[] trip);
     public void addService(Service service);
     public void removeService(Service service);
-    public Booking book(specialNeeds sn, User user);
+    public Booking book(specialNeeds sn, Service service, User user);
 }
