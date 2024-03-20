4f9b93f0212103ccc32736f7cb527ce140d09e19
Alexandre Klein <alexandre.klein04@protonmail.com>
2024-03-20 15:51:58 +0000

    Moved all the testing infrastructure to a separate directory

---
+++ Assignment 4/src/Tests/MockObjects/MockHotel.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+public class MockHotel extends Service{
+  private LocalDateTime checkIn;
+  private LocalDateTime checkOut;
+  public LocalDateTime getCheckIn(){return checkIn;}
+  public LocalDateTime getCheckOut(){return checkOut;}
+  public void SetCheckIn(LocalDateTime checkin){ this.checkIn = checkin; }
+  public void SetCheckOut(LocalDateTime checkOut){ this.checkOut = checkOut; }
+}
