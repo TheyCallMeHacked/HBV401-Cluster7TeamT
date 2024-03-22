4ff3ccfaddd199e6dc35b040328689ea0aca5103
lumeto_de_diamanto <gretar.vidir@gmail.com>
2024-03-22 11:22:31 +0000

    Added recommend and sort tests

--- Assignment 4/src/TestFixture.java
+++ Assignment 4/src/TestFixture.java
@@ -23,13 +23,55 @@ public class TestFixture {
     }
 
     @Test
-    void anotherThing() {
-        // Code that tests another thing
+    void recommend() {
+        Trip trip = new Trip();
+        Service[] result = engine.recommend(trip);
+        assertEquals(result, new Service[0]);
     }
 
     @Test
-    void somethingElse() {
-        // Code that tests something else
+    void sortId() {
+        Service[] toBeSorted = new Service[3];
+        toBeSorted[0] = new Service();
+        toBeSorted[0].setId(1)
+        toBeSorted[1] = new Service();
+        toBeSorted[1].setId(0)
+        toBeSorted[2] = new Service();
+        toBeSorted[2].setId(2)
+        toBeSorted = engine.sort(toBeSorted, SortingParam.ID, false);
+        assertEquals(toBeSorted[0].getId(), 0);
+        assertEquals(toBeSorted[1].getId(), 1);
+        assertEquals(toBeSorted[2].getId(), 2);
+    }
+
+    @Test
+    void sortPrice() {
+        Service[] toBeSorted = new Service[3];
+        toBeSorted[0] = new Service();
+        toBeSorted[0].setPrice(1)
+        toBeSorted[1] = new Service();
+        toBeSorted[1].setPrice(0)
+        toBeSorted[2] = new Service();
+        toBeSorted[2].setPrice(2)
+        toBeSorted = engine.sort(toBeSorted, SortingParam.PRICE, false);
+        assertEquals(toBeSorted[0].getPrice(), 0);
+        assertEquals(toBeSorted[1].getPrice(), 1);
+        assertEquals(toBeSorted[2].getPrice(), 2);
+    }
+
+    @Test
+    void sortIdReverse() {
+        Service[] toBeSorted = new Service[3];
+        toBeSorted[0] = new Service();
+        toBeSorted[0].setId(1)
+        toBeSorted[1] = new Service();
+        toBeSorted[1].setId(0)
+        toBeSorted[2] = new Service();
+        toBeSorted[2].setId(2)
+        toBeSorted = engine.sort(toBeSorted, SortingParam.ID, true);
+        assertEquals(toBeSorted[0].getId(), 2);
+        assertEquals(toBeSorted[1].getId(), 1);
+        assertEquals(toBeSorted[2].getId(), 0);
     }
 
     @AfterEach
